b67a0001f94e766b2fff2972157d57ce1bac3a64
Mark Glenn <markglenn@gmail.com>
2011-06-25 15:16:51 -0500

    Specs no longer work.  Removing.  Added gemfile.

--- dotnet.watchr.rb
+++ dotnet.watchr.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 # This class helps call growlnotify with the given parameters.
 class GrowlNotifier
 
@@ -22,36 +24,10 @@ def projects_for_file( file )
   `grep -RHl --include "*.csproj" "#{regex}" *`.split
 end
 
-class CodeFinder
-  def initialize( file )
-    @file = file
-  end
-
-  def project_file
-    projects_for_file( @file ).first
-  end
-
-  def test_project
-    project = File.basename(project_file,'.csproj')
-    Dir.glob("**/#{project}.Tests.csproj").first
-  end
-
-  def test_output
-    proj = test_project
-
-    return nil if proj.nil?
-    File.join( File.dirname(proj), 'bin', 'Debug', File.basename(proj, '.csproj') + '.dll' )
-  end
-end
-
-GrowlNotifier.notify 'Watchr Loaded', 'System ready'
-
 watch( '.*\.cs' ) do |cs|
   projects_for_file(cs[0]).each do |f|
     GrowlNotifier.notify 'Building Project', "Building #{f}"
-    results = `xbuild /nologo /verbosity:quiet #{f}`.split("\n")
-
-    puts results
+    puts ( results = `xbuild /nologo /verbosity:quiet #{f}`.split("\n") )
 
     warnings = results.select{|r| r.include?('warning') }.length
     errors = results.select{|r| r.include?('error') }.length
@@ -62,27 +38,16 @@ watch( '.*\.cs' ) do |cs|
   end
 end
 
-watch('.*/bin/.*dll11$') do |md|
-  puts projects_for_file(md[0])
-  return
-  puts "#{md} changed"
-  test_dll = CodeFinder.new( md[0] ).test_output
-
-  if test_dll
-    puts "Running tests in #{test_dll}"
-    puts
-    puts output = `nunit-console4 #{test_dll} -noshadow -nologo`
-
-    /Tests run: (\d+), Failures: (\d+), Not run: (\d+)/.match(output) do |m|
-      tests = m[1].to_i
-      failures = m[2].to_i
-      skipped = m[3].to_i
-
-      if failures == 0
-        GrowlNotifier.notify( 'Succeeded', 'All tests passed!' )
-      else
-        GrowlNotifier.notify( 'Failed', "#{failures} failures" )
-      end
-    end
-  end
+# --------------------------------------------------
+# Signal Handling
+# --------------------------------------------------
+# Ctrl-\
+Signal.trap('QUIT') do
+  puts " --- Running all tests ---\n\n"
+  #run_all_tests
 end
+
+# Ctrl-C
+Signal.trap('INT') { abort("\n") }
+
+GrowlNotifier.notify 'Watchr Loaded', 'System ready'
